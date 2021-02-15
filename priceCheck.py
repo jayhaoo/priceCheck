4231e0de129b8310917564f73419a75e3964e7d6
Jason Hao <jasonhao@jasons-air.lan>
2021-02-14 18:57:13 -0800

    Working on it

--- priceCheck.py
+++ priceCheck.py
@@ -1,7 +1,36 @@
+import sys
+import os.path
+from os import path
 
+def parse_text_file(textFile):
+	print('Textfile: ', textFile)
+	f = open(textFile, 'r')
+	for item in f:
+		print(item)
+		items = item.split(' ')
+		print(items[0])
+		print(len(items))
 
 def main():
-	print('Hello World!')
+	if len(sys.argv) != 2:
+		print('ERROR: expecting textfile')
+		print('usage: python3 priceCheck.py [\'example.txt\']')
+		quit()
+
+	textFile = sys.argv[1]
+
+	if path.exists(textFile) == False:
+		print('ERROR: ', textFile, 'does not exist')
+		quit()
+	elif path.isfile(textFile) == False:
+		print('ERROR: ', textFile, ' is not a file.')
+		quit()
+	elif textFile.endswith('.txt') == False:
+		print('ERROR: ', textFile, ' is not a text file')
+		quit()
+
+	parse_text_file(textFile)
+
 
 if __name__ == "__main__":
 	main()
